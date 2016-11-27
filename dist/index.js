ee476c8d37fca0df21d2a4b9afebe8a9ae6a8c48
radarsu <rkrohmorath@gmail.com>
2016-11-27 21:38:03 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -1,89 +1,85 @@
 "use strict";
 const _ = require("lodash");
-let replace = (data) => {
-    data = _.cloneDeep(data);
-    let specialChar = exports.CircularJSON._config.specialChar;
-    let seenObjectsDict = {
-        [specialChar]: data,
-    };
-    let path = [""];
-    let replaceRecursive = (currentData) => {
-        _.forOwn(currentData, (value, key) => {
-            if (typeof value !== "object") {
-                return;
-            }
-            let foundKey = _.findKey(seenObjectsDict, (object) => {
-                return object === value;
-            });
-            if (foundKey) {
-                currentData[key] = foundKey;
-            }
-            else {
-                let thisPath = `${path.join(specialChar)}${specialChar}${key}`;
-                seenObjectsDict[thisPath] = value;
-                let pathBefore = _.clone(path);
-                path.push(key);
-                currentData[key] = replaceRecursive(value);
-                path = pathBefore;
-            }
-        });
-        return currentData;
-    };
-    return replaceRecursive(data);
-};
-let refer = (data, root = data) => {
-    let specialChar = exports.CircularJSON._config.specialChar;
-    let leaveRefIfUndefined = exports.CircularJSON._config.leaveRefIfUndefined;
-    let referRecursive = (currentData) => {
-        _.forOwn(currentData, (value, key) => {
-            if (typeof value === "object") {
-                return referRecursive(value);
-            }
-            if (typeof value !== "string" || value.charAt(0) !== specialChar) {
-                return;
-            }
-            let path = value.split(specialChar);
-            path.shift();
-            if (path[0] === "") {
-                currentData[key] = root;
-                return;
-            }
-            currentData[key] = _.get(root, path);
-            if (leaveRefIfUndefined && typeof currentData[key] === "undefined") {
-                currentData[key] = value;
-            }
-        });
-        return currentData;
-    };
-    return referRecursive(data);
-};
 exports.CircularJSON = {
     _config: {
         leaveRefIfUndefined: true,
-        space: 0,
         specialChar: "~",
     },
+    _default: {
+        space: 0,
+    },
     config: (options = {}) => {
-        this._config = options;
+        _.assign(exports.CircularJSON._config, options);
     },
     parse: (creatorOptions) => {
         let options = _.defaults(creatorOptions || {}, {
             leaveRefIfUndefined: exports.CircularJSON._config.leaveRefIfUndefined,
             specialChar: exports.CircularJSON._config.specialChar,
         });
-        if (typeof options.value !== "string") {
-            options.value = exports.CircularJSON.stringify({
-                value: options.value,
+        return exports.CircularJSON.regenerate(JSON.parse(options.value), options.root);
+    },
+    regenerate: (data, root = data) => {
+        let specialChar = exports.CircularJSON._config.specialChar;
+        let leaveRefIfUndefined = exports.CircularJSON._config.leaveRefIfUndefined;
+        let referRecursive = (currentData) => {
+            _.forOwn(currentData, (value, key) => {
+                if (typeof value === "object") {
+                    return referRecursive(value);
+                }
+                if (typeof value !== "string" || value.charAt(0) !== specialChar) {
+                    return;
+                }
+                let path = value.split(specialChar);
+                path.shift();
+                if (path[0] === "") {
+                    currentData[key] = root;
+                    return;
+                }
+                currentData[key] = _.get(root, path);
+                if (leaveRefIfUndefined && typeof currentData[key] === "undefined") {
+                    currentData[key] = value;
+                }
             });
-        }
-        return refer(JSON.parse(options.value), options.root);
+            return currentData;
+        };
+        return referRecursive(data);
+    },
+    replace: (data) => {
+        data = _.cloneDeep(data);
+        let specialChar = exports.CircularJSON._config.specialChar;
+        let seenObjectsDict = {
+            [specialChar]: data,
+        };
+        let path = [""];
+        let replaceRecursive = (currentData) => {
+            _.forOwn(currentData, (value, key) => {
+                if (typeof value !== "object") {
+                    return;
+                }
+                let foundKey = _.findKey(seenObjectsDict, (object) => {
+                    return object === value;
+                });
+                if (foundKey) {
+                    currentData[key] = foundKey;
+                }
+                else {
+                    let thisPath = `${path.join(specialChar)}${specialChar}${key}`;
+                    seenObjectsDict[thisPath] = value;
+                    let pathBefore = _.clone(path);
+                    path.push(key);
+                    currentData[key] = replaceRecursive(value);
+                    path = pathBefore;
+                }
+            });
+            return currentData;
+        };
+        return replaceRecursive(data);
     },
     stringify: (creatorOptions) => {
         let options = _.defaults(creatorOptions || {}, {
-            space: exports.CircularJSON._config.space,
-            specialChar: exports.CircularJSON._config.specialChar,
+            space: exports.CircularJSON._default.space,
         });
-        return JSON.stringify(replace(options.value), options.replacer, options.space);
+        return JSON.stringify(exports.CircularJSON.replace(options.value), options.replacer, options.space);
     },
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as _ from "lodash";
 
-export interface CircularJSONConfig {
+export interface CircularJSONConfigQuery {
     specialChar?: string;
+    leaveRefIfUndefined?: boolean;
 }
 
 export interface CircularJSONStringifyOptions {
@@ -11,101 +12,21 @@ export interface CircularJSONStringifyOptions {
 }
 
 export interface CircularJSONParseOptions {
-    value: string | {};
+    value: string;
     root?: {};
     leaveRefIfUndefined?: boolean;
 }
 
-let replace = (data: any) => {
-
-    data = _.cloneDeep(data);
-
-    let specialChar = CircularJSON._config.specialChar;
-
-    let seenObjectsDict = {
-        [specialChar]: data,
-    };
-
-    let path: string[] = [""];
-
-    let replaceRecursive = (currentData: any) => {
-        _.forOwn(currentData, (value: any, key: string) => {
-            if (typeof value !== "object") {
-                return;
-            }
-
-            // check if object is in seenObjects
-            let foundKey = _.findKey(seenObjectsDict, (object: any) => {
-                return object === value;
-            });
-
-            if (foundKey) {
-                currentData[key] = foundKey;
-            } else {
-                let thisPath = `${path.join(specialChar)}${specialChar}${key}`;
-
-                // not found - add it
-                seenObjectsDict[thisPath] = value;
-
-                // save previous path
-                let pathBefore = _.clone(path);
-                path.push(key);
-                currentData[key] = replaceRecursive(value);
-                // revert path
-                path = pathBefore;
-            }
-        });
-
-        return currentData;
-    };
-
-    return replaceRecursive(data);
-};
-
-let refer = (data: any, root: any = data) => {
-
-    let specialChar = CircularJSON._config.specialChar;
-    let leaveRefIfUndefined = CircularJSON._config.leaveRefIfUndefined;
-
-    let referRecursive = (currentData: any) => {
-        _.forOwn(currentData, (value: any, key: string) => {
-            if (typeof value === "object") {
-                return referRecursive(value);
-            }
-
-            // check if value ~like~this
-            if (typeof value !== "string" || value.charAt(0) !== specialChar) {
-                return;
-            }
-
-            let path = value.split(specialChar);
-            path.shift();
-            if (path[0] === "") {
-                currentData[key] = root;
-                return;
-            }
-
-            currentData[key] = _.get(root, path);
-
-            if (leaveRefIfUndefined && typeof currentData[key] === "undefined") {
-                // leave ref
-                currentData[key] = value;
-            }
-        });
-        return currentData;
-    };
-
-    return referRecursive(data);
-};
-
 export const CircularJSON = {
     _config: {
         leaveRefIfUndefined: true,
-        space: 0,
         specialChar: "~",
     },
-    config: (options: CircularJSONConfig = {}) => {
-        this._config = options;
+    _default: {
+        space: 0,
+    },
+    config: (options: CircularJSONConfigQuery = {}) => {
+        _.assign(CircularJSON._config, options);
     },
     parse: (creatorOptions: CircularJSONParseOptions) => {
         let options = _.defaults(creatorOptions || {}, {
@@ -113,19 +34,96 @@ export const CircularJSON = {
             specialChar: CircularJSON._config.specialChar,
         });
 
-        if (typeof options.value !== "string") {
-            options.value = CircularJSON.stringify({
-                value: options.value,
-            });
-        }
-
-        return refer(JSON.parse(<string>options.value), options.root);
+        return CircularJSON.regenerate(JSON.parse(options.value), options.root);
     },
+    // restore circular references from object
+    regenerate: (data: {}, root: {} = data) => {
+
+        let specialChar = CircularJSON._config.specialChar;
+        let leaveRefIfUndefined = CircularJSON._config.leaveRefIfUndefined;
+
+        let referRecursive = (currentData: any) => {
+            _.forOwn(currentData, (value: any, key: string) => {
+                if (typeof value === "object") {
+                    return referRecursive(value);
+                }
+
+                // check if value ~like~this
+                if (typeof value !== "string" || value.charAt(0) !== specialChar) {
+                    return;
+                }
+
+                let path = value.split(specialChar);
+                path.shift();
+                if (path[0] === "") {
+                    currentData[key] = root;
+                    return;
+                }
+
+                currentData[key] = _.get(root, path);
+
+                if (leaveRefIfUndefined && typeof currentData[key] === "undefined") {
+                    // leave ref
+                    currentData[key] = value;
+                }
+            });
+            return currentData;
+        };
+
+        return referRecursive(data);
+
+    },
+    // just replace circular references with strings
+    replace: (data: {}) => {
+
+        data = _.cloneDeep(data);
+
+        let specialChar = CircularJSON._config.specialChar;
+
+        let seenObjectsDict = {
+            [specialChar]: data,
+        };
+
+        let path: string[] = [""];
+
+        let replaceRecursive = (currentData: any) => {
+            _.forOwn(currentData, (value: any, key: string) => {
+                if (typeof value !== "object") {
+                    return;
+                }
+
+                // check if object is in seenObjects
+                let foundKey = _.findKey(seenObjectsDict, (object: any) => {
+                    return object === value;
+                });
+
+                if (foundKey) {
+                    currentData[key] = foundKey;
+                } else {
+                    let thisPath = `${path.join(specialChar)}${specialChar}${key}`;
+
+                    // not found - add it
+                    seenObjectsDict[thisPath] = value;
+
+                    // save previous path
+                    let pathBefore = _.clone(path);
+                    path.push(key);
+                    currentData[key] = replaceRecursive(value);
+                    // revert path
+                    path = pathBefore;
+                }
+            });
+
+            return currentData;
+        };
+
+        return replaceRecursive(data);
+    },
+    // replaces and stringifies
     stringify: (creatorOptions: CircularJSONStringifyOptions) => {
         let options = _.defaults(creatorOptions || {}, {
-            space: CircularJSON._config.space,
-            specialChar: CircularJSON._config.specialChar,
+            space: CircularJSON._default.space,
         });
-        return JSON.stringify(replace(options.value), options.replacer, options.space);
+        return JSON.stringify(CircularJSON.replace(options.value), options.replacer, options.space);
     },
 };
